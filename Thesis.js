7f412b0a00511f0df853fade2982b8667520cc63
NYChiPASu <97855116+NYChiPASu@users.noreply.github.com>
2022-03-01 21:35:39 -0600

    Add files via upload

--- Thesis.js
+++ Thesis.js
@@ -1,3 +1,8 @@
+function start(){
+    loadXML();
+    mapLoad();
+}
+
 function showMinute(){
     var currentMinute = new Date().getMinutes();
     console.log(currentMinute);
@@ -64,6 +69,44 @@ function takeInput(){
     });
     selectedElement.innerText=array1.sort();
 }
+
+function mapLoad(){
+    var latLng = [41.789649, -87.599702];
+    //set 
+    var mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+    var mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibnljaGlwYXN1IiwiYSI6ImNsMDFuejJpaTA1ZXMzam1qOGs2MDdzeG4ifQ.vxLKFyZ3DnJfngaUmChCuQ';
+    //
+    var streets = L.tileLayer(mbUrl, {
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset:-1,
+        attribution:mbAttr
+    });
+    //
+    var map=L.map('map',{
+        center:latLng,
+        zoom:16,
+        layers: [streets]
+    });
+    //
+    var baseLayers={
+        "Grayscale": grayscale,
+        "Streets": streets
+    };
+    L.control.layers(baseLayers).addTo(map);
+    L.marker(latLng).addTo(map).bindPopup("<b>UChicago<br>Campus</br>").openPopup();
+    //
+    var popup = L.popup();
+    function onMapClick(e){
+        popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at "+ e.latlng.toString())
+        .openOn(map);
+    }
+    map.on('click', onMapClick);
+    //
+}
+
 
 function wikiAPI(){
     var searchTerm = document.getElementById("searchTerm").value;
